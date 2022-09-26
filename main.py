bca090da34483fa50bba6f59aae7c76c1030f22a
eba-alemayehu <31594849+eba-alemayehu@users.noreply.github.com>
2022-09-26 11:01:33 +0300

    cbe birr packaged

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     cbebirr.push_ussd(thirdPartyID='serdo', primaryPartyIdentifier='251910867889', receiverPartyIdentifier='369589',
                       intiatorIdentifier='Eba01', securityCredential='A30pWRG8depVJtCDLpnSpg==',
                       posDeviceID='POS234789', shortCode='369589', amount='10', password='MEFgDcvzQdY=',
-                      timestamp='20150101010101', resultURL='http://equbde.com:31794/dequbcbedigital/result/', )
+                      timestamp='20150101010101', resultURL='https://plain-falcons-trade-196-188-55-25.loca.lt/cbebirr/', )
 
     # See PyCharm help at https://www.jetbrains.com/help/pycharm/
