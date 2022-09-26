968412f3e3c035c7a2dd4637dd8f72ee15b7bcc2
eba-alemayehu <31594849+eba-alemayehu@users.noreply.github.com>
2022-09-26 10:28:58 +0300

    cbe birr packaged

---
+++ cbebirr.py
@@ -0,0 +1,92 @@
+import json
+
+import requests
+import xmltodict
+
+
+def push_ussd(thirdPartyID,
+              password,
+              resultURL,
+              timestamp,
+              intiatorIdentifier,
+              securityCredential,
+              shortCode,
+              primaryPartyIdentifier,
+              receiverPartyIdentifier,
+              amount,
+              posDeviceID,
+              currency='ETB', url="http://172.30.10.3:8081/payment/services/APIRequestMgrService"):
+    request = """
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:api="http://cps.huawei.com/cpsinterface/api_requestmgr" xmlns:req="http://cps.huawei.com/cpsinterface/request" xmlns:com="http://cps.huawei.com/cpsinterface/common">
+       <soapenv:Header/>
+       <soapenv:Body>
+          <api:Request>
+             <req:Header>
+                <req:Version>1.0</req:Version>
+                <req:CommandID>InitTrans_C2B Online Payment Request</req:CommandID>
+                <req:OriginatorConversationID>S_X20171211143322046</req:OriginatorConversationID>
+                <req:Caller>
+                   <req:CallerType>2</req:CallerType>
+                   <req:ThirdPartyID>{thirdPartyID}</req:ThirdPartyID>
+                   <req:Password>{password}=</req:Password>
+                   <req:ResultURL>{resultURL}</req:ResultURL>
+                </req:Caller>
+                <req:KeyOwner>1</req:KeyOwner>
+                <req:Timestamp>{timestamp}</req:Timestamp>
+             </req:Header>
+             <req:Body>
+                <req:Identity>
+                   <req:Initiator>
+                      <req:IdentifierType>11</req:IdentifierType>
+                      <req:Identifier>{intiatorIdentifier}</req:Identifier>
+                      <req:SecurityCredential>{securityCredential}</req:SecurityCredential>
+                      <req:ShortCode>{shortCode}</req:ShortCode>
+                   </req:Initiator>
+                   <req:PrimaryParty>
+                      <req:IdentifierType>1</req:IdentifierType>
+                      <req:Identifier>{primaryPartyIdentifier}</req:Identifier>
+                   </req:PrimaryParty>
+                   <req:ReceiverParty>
+                      <req:IdentifierType>4</req:IdentifierType>
+                      <req:Identifier>{receiverPartyIdentifier}</req:Identifier>
+                   </req:ReceiverParty>
+                </req:Identity>
+                <req:TransactionRequest>
+                   <req:Parameters>
+                      <req:Amount>{amount}</req:Amount>
+                      <req:Currency>{currency}</req:Currency>
+                   </req:Parameters>
+                </req:TransactionRequest>
+                <req:ReferenceData>
+                   <req:ReferenceItem>
+                      <com:Key>POSDeviceID</com:Key>
+                      <com:Value>{posDeviceID}</com:Value>
+                   </req:ReferenceItem>
+                </req:ReferenceData>
+             </req:Body>
+          </api:Request>
+       </soapenv:Body>
+    </soapenv:Envelope>
+    """.format(
+        thirdPartyID=thirdPartyID,
+        password=password,
+        resultURL=resultURL,
+        timestamp=timestamp,
+        intiatorIdentifier=intiatorIdentifier,
+        securityCredential=securityCredential,
+        shortCode=shortCode,
+        primaryPartyIdentifier=primaryPartyIdentifier,
+        receiverPartyIdentifier=receiverPartyIdentifier,
+        amount=amount,
+        currency=currency,
+        posDeviceID=posDeviceID
+    )
+    print(request)
+    headers = {'Content-Type': 'text/xml; charset=UTF-8'}
+    response = requests.post(url=url, data=request, headers=headers)
+    print(response.content)
+
+    decoded_response = response.content.decode('utf-8')
+    response_json = json.loads(json.dumps(xmltodict.parse(decoded_response)))
+    print(response_json)
+    return response_json
--- main.py
+++ main.py
@@ -1,71 +1,9 @@
-import json
+import cbebirr
 
-import requests
-import xmltodict
-
-request = """<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:api="http://cps.huawei.com/cpsinterface/api_requestmgr" xmlns:req="http://cps.huawei.com/cpsinterface/request" xmlns:com="http://cps.huawei.com/cpsinterface/common">
-   <soapenv:Header/>
-   <soapenv:Body>
-      <api:Request>
-         <req:Header>
-            <req:Version>1.0</req:Version>
-            <req:CommandID>InitTrans_C2B Online Payment Request</req:CommandID>
-            <req:OriginatorConversationID>S_X20171211143322046</req:OriginatorConversationID>
-            <req:Caller>
-               <req:CallerType>2</req:CallerType>
-               <req:ThirdPartyID>serdo</req:ThirdPartyID>
-               <req:Password>MEFgDcvzQdY=</req:Password>
-               <req:ResultURL>http://equbde.com:31794/dequbcbedigital/result/</req:ResultURL>
-            </req:Caller>
-            <req:KeyOwner>1</req:KeyOwner>
-            <req:Timestamp>20150101010101</req:Timestamp>
-         </req:Header>
-         <req:Body>
-            <req:Identity>
-               <req:Initiator>
-                  <req:IdentifierType>11</req:IdentifierType>
-                  <req:Identifier>Eba01</req:Identifier>
-                  <req:SecurityCredential>A30pWRG8depVJtCDLpnSpg==</req:SecurityCredential>
-                  <req:ShortCode>369589</req:ShortCode>
-               </req:Initiator>
-               <req:PrimaryParty>
-                  <req:IdentifierType>1</req:IdentifierType>
-                  <req:Identifier>251985028197</req:Identifier>
-               </req:PrimaryParty>
-               <req:ReceiverParty>
-                  <req:IdentifierType>4</req:IdentifierType>
-                  <req:Identifier>369589</req:Identifier>
-               </req:ReceiverParty>
-            </req:Identity>
-            <req:TransactionRequest>
-               <req:Parameters>
-                  <req:Amount>10.0</req:Amount>
-                  <req:Currency>ETB</req:Currency>
-               </req:Parameters>
-            </req:TransactionRequest>
-            <req:ReferenceData>
-               <req:ReferenceItem>
-                  <com:Key>POSDeviceID</com:Key>
-                  <com:Value>POS234789</com:Value>
-               </req:ReferenceItem>
-            </req:ReferenceData>
-         </req:Body>
-      </api:Request>
-   </soapenv:Body>
-</soapenv:Envelope>"""
-
-def print_hi(name):
-    headers = {'Content-Type': 'text/xml; charset=UTF-8'}
-    url = 'http://172.30.10.3:8081/payment/services/APIRequestMgrService'
-    response = requests.post(url=url, data=request, headers=headers)
-    print(response.content)
-
-    decoded_response = response.content.decode('utf-8')
-    response_json = json.loads(json.dumps(xmltodict.parse(decoded_response)))
-    print(response_json)
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    cbebirr.push_ussd(thirdPartyID='serdo', primaryPartyIdentifier='251910867889', receiverPartyIdentifier='369589',
+                      intiatorIdentifier='Eba01', securityCredential='A30pWRG8depVJtCDLpnSpg==',
+                      posDeviceID='POS234789', shortCode='369589', amount='10', password='MEFgDcvzQdY=',
+                      timestamp='20150101010101', resultURL='http://equbde.com:31794/dequbcbedigital/result/', )
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
